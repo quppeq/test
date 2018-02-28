a245a80095f7727054ea521b3aa173905c3e854e
Борис Фомський < >
2018-02-28 23:51:01 +0200

    taks taks to commit

---
+++ novator.py
@@ -0,0 +1,57 @@
+import config
+import telebot
+from telebot import types
+import shelve
+import random
+import requests
+bot = telebot.TeleBot(config.token)
+
+
+
+
+@bot.message_handler(commands=["start"])
+def mess(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row('Аніме', 'Вихід')
+   # markup.row("Розклад", "Записати розклад")
+    bot.send_message(message.chat.id, "Погнали", reply_markup=markup)
+
+
+@bot.message_handler(regexp="Вихід")
+def mess(message):
+    markup = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, text="ok", reply_markup=markup)
+
+@bot.message_handler(regexp="Новатор запиши")
+def messs(message):
+    #bot.send_message(message.chat.id, message.text)
+    with shelve.open("dict.txt") as file:
+        for i in file.keys():
+            if i in message.text:
+                file[i] = message.text
+
+
+@bot.message_handler(regexp="розклад")
+def mess(message):
+    #bot.send_message(message.chat.id, message.text)
+    text = ""
+    with shelve.open("dict.txt") as file:
+        for i in file.keys():
+            text += "**{}** \n{}".format(i, file[i])
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=["pidor", "help"])
+def mess(message):
+    bot.send_message(message.chat.id, "{}, ти підор".format(message.chat.first_name))
+
+
+@bot.message_handler(regexp="Аніме")
+def mess(message):
+    r = requests.get("https://danbooru.donmai.us/posts/"+str(random.randrange(3035774)))
+    config.html.feed(r.text)
+    bot.send_message(message.chat.id, config.html.url)
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
